55fa2a46a3f8d8dadc7dcdd7832cf95ac8a539d4
asciijiang <52809614+asciijiang@users.noreply.github.com>
2019-07-14 00:31:49 +0800

    Update iterable.py

--- python-advance-programming/generator/iterable.py
+++ python-advance-programming/generator/iterable.py
@@ -1,1 +1,14 @@
-print("generator")
+from collections.abc import Iterable,Iterator
+a = [1,2]
+print(isinstance(a,Iterable))
+print(isinstance(a,Iterator))
+
+'''
+D:\用户目录\我的文档\GitHub\p
+>python iterable.py
+True
+False
+
+D:\用户目录\我的文档\GitHub\p
+
+'''

d2070af348b39bc8672a4756c3b9b6fd83be12dd
asciijiang <52809614+asciijiang@users.noreply.github.com>
2019-07-14 01:09:00 +0800

    Update iterable.py

--- python-advance-programming/generator/iterable.py
+++ python-advance-programming/generator/iterable.py
@@ -1,13 +1,14 @@
 from collections.abc import Iterable,Iterator
 a = [1,2]
+iter_rator = iter(a)
 print(isinstance(a,Iterable))
-print(isinstance(a,Iterator))
+print(isinstance(iter_rator,Iterator))
 
 '''
 D:\用户目录\我的文档\GitHub\p
 >python iterable.py
 True
-False
+True
 
 D:\用户目录\我的文档\GitHub\p
 

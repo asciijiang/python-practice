f8c61945a98c1dcadd5d9f1fb1e14c47c26abfde
asciijiang <52809614+asciijiang@users.noreply.github.com>
2019-07-15 12:34:59 +0800

    Update socket_client.py

--- python-advance-programming/socket/socket_client.py
+++ python-advance-programming/socket/socket_client.py
@@ -1,9 +1,11 @@
 import socket
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(('127.0.0.1',8000))
-while True:
-	data = client.recv(1024)
-	print(data.decode("utf8"))
+while True:	
 	re_data = input()
 	client.send(re_data.encode("utf8"))
+	if re_data == "exit":
+		break
+	data = client.recv(1024)
+	print(data.decode("utf8"))
 client.close()

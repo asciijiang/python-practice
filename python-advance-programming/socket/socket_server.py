589f2c8af3e8a0fef940e95cd021ee8383e60261
asciijiang <52809614+asciijiang@users.noreply.github.com>
2019-07-15 12:35:10 +0800

    Update socket_server.py

--- python-advance-programming/socket/socket_server.py
+++ python-advance-programming/socket/socket_server.py
@@ -1,18 +1,29 @@
 import socket
+import threading
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(('0.0.0.0',8000))
 server.listen()
-sock,addr = server.accept()
+
+def handle_sock(sock,addr):
+	while True:
+		data = sock.recv(1024)
+		print(data.decode("utf8"))
+		if data.decode("utf8") == "exit":
+			break
+		re_data = input()
+		sock.send(re_data.encode("utf8"))
+	sock.close()	
 
 #获取从客户端发送的数据
 #一次获取1k的数据
 while True:
-	data = sock.recv(1024)
-	print(data.decode("utf8"))
-	re_data = input()
-	sock.send(re_data.encode("utf8"))
-#特定socket关闭
-sock.close()
-#server监听socket关闭
-server.close()
+	sock,addr = server.accept()
+	
+	client_thread = threading.Thread(target=handle_sock,args=(sock,addr))
+	client_thread.start()
+	
+# #特定socket关闭
+# sock.close()
+# #server监听socket关闭
+# server.close()

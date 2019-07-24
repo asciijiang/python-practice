5af1b6da7f5bb7c660c1fcc934a7317f70373809
asciijiang <52809614+asciijiang@users.noreply.github.com>
2019-07-24 16:07:24 +0800

    Update yield_from_test.py

--- python-advance-programming/coroutine/yield_from_test.py
+++ python-advance-programming/coroutine/yield_from_test.py
@@ -6,10 +6,31 @@ my_dict = {
 	"bobby2":"http://www.imooc.com",
 }
 
-def my_chain(*args,**kwargs):
-	for my_iterable in args:
-		for value in my_iterable:
-			yield value
+# def g1(iterable):
+	# yield iterable
+	
+# def g2(iterable):
+	# yield from iterable
 
-for value in my_chain( my_list,my_dict,range(5,10) ):
-	print(value)
+# for value in g1(range(10)):
+	# print(value)
+	
+# for value in g2(range(10)):
+	# print(value)
+
+# def my_chain(*args,**kwargs):
+	# for my_iterable in args:
+		# yield from my_iterable
+		# # for value in my_iterable:
+			# # yield value
+
+# for value in my_chain( my_list,my_dict,range(5,10) ):
+	# print(value)
+	
+# print( type( range(5,10) ) )
+
+def g1(gen):
+	yield from gen
+
+def main():
+	g = g1()
